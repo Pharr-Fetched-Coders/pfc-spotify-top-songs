e550a3293c070292b9f607654aefe4b1ddd2c429
garysteele777 <155407816+garysteele777@users.noreply.github.com>
2024-11-29 20:31:31 -0600

    Its working .. Great

--- chatgpt_spotify.py
+++ chatgpt_spotify.py
@@ -1,17 +1,23 @@
+# Branch: gary-readJSON
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
-
-# This file in my new branch
+import json
+import textwrap
+from rich import print
+from rich.console import Console
+from gtts import gTTS
+import requests
 
 load_dotenv()   # Import environment variables from .env file
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 USER_ID = os.getenv("SPOTIFY_USER_ID")
 REDIRECT_URI = "http://localhost:8888/callback"
-print(f"My Spotify User ID = {USER_ID}")
+# print(f"My Spotify User ID = {USER_ID}")
+print()
 # Scopes required for the program
 SCOPES = "user-read-playback-state user-modify-playback-state user-read-private"
 
@@ -35,7 +41,7 @@ def search_track(sp, artist_name, track_name):
     results = sp.search(q=query, type="track", limit=1)
     if results["tracks"]["items"]:
         track = results["tracks"]["items"][0]
-        print(f"Found track: {track['name']} by {track['artists'][0]['name']}")
+        # print(f"Found track: {track['name']} by {track['artists'][0]['name']}")
         return track["id"]
     else:
         print("Track not found!")
@@ -47,30 +53,69 @@ def play_track(sp, track_id):
     devices = sp.devices()
     # Print all devices for debugging
     for device in devices['devices']:
-        print(f"Name: {device['name']}, ID: {device['id']}, Active: {device['is_active']}")
+        # print(f"Name: {device['name']}, ID: {device['id']}, Active: {device['is_active']}")
         my_device_id = device['id']
 
     if devices["devices"]:
         active_device = devices["devices"][0]
-        print(f"Playing on device: {active_device['name']}")
+        # print(f"Playing on device: {active_device['name']}")
         sp.start_playback(device_id=my_device_id, uris=[f"spotify:track:{track_id}"])
     else:
         print("No active devices found. Please open Spotify on one of your devices.")
+
+def print_track_description(all_songs, genre, song_index):
+
+    # Print out the top header
+    console = Console(width=60)
+    print()
+    console.rule("[bold red]Description Start")
+    print()
+
+    # Print out the track description
+    wrapped_text = textwrap.fill(all_songs[genre][song_index]['description'], width=50)
+    console.print(wrapped_text, justify="center")
+    print()
+
+    # Print out the bottom header
+    console.rule("[bold red]Description End")
+    print()
+
 
 def main():
     # Step 1: Authenticate and get the Spotipy client
     sp = authenticate_spotify()
 
+    # initial_setup of json
+    file_name = '1960s.json'
+    decade = file_name[:5]
+    # Was getting error with ' char, needed to set this encoding
+    with open(file_name, 'r', encoding="utf-8") as f:
+        all_songs = json.load(f)
+
+    for song in all_songs["soft_rock"]:
+        artist_name = song["artist"]
+        track_name = song["song_name"]
+        print(f"{song['rank']:>5}   {song['year']:<2}   {artist_name:<30}   {track_name}")
+        print_track_description(all_songs, "soft_rock", song["rank"]-1)
+
+        input("Press Enter to Start Playing Track . . .")
+        print()
+
+        track_id = search_track(sp, artist_name, track_name)
+        play_track(sp, track_id)
+        input("Press Enter to Go On to next Track. . .")
+        print()
+
     # Step 2: Get user input for artist and song title
-    artist_name = input("Enter artist name: ")
-    track_name = input("Enter song title: ")
+    # artist_name = input("Enter artist name: ")
+    # track_name = input("Enter song title: ")
 
     # Step 3: Search for the track
-    track_id = search_track(sp, artist_name, track_name)
-    print(track_id)
-    if track_id:
+    # track_id = search_track(sp, artist_name, track_name)
+    # print(track_id)
+    # if track_id:
         # Step 4: Play the track
-        play_track(sp, track_id)
+        # play_track(sp, track_id)
 
 if __name__ == "__main__":
     main()
